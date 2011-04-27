bb30a62de64880ff6c1dc6d0dff5d78b378e7206
Manuel Silvoso <msilvoso@gmail.com>
2011-04-27 18:01:03 +0200

    file headers with copyright changed

--- public/index.php
+++ public/index.php
@@ -1,16 +1,22 @@
 <?php
-	/*******************************************************************************
-	Copyright 2010-2011 CASES Luxembourg (www.cases.lu)
-	Copyright 2011 SMILE GIE
+        /*******************************************************************************
+        Copyright 2011 CASES Luxembourg (www.cases.lu)
+        Copyright 2011 SMILE GIE
 
-	   Licensed under the Apache License, Version 2.0 (the "License");
-	   you may not use this file except in compliance with the License.
-	   You may obtain a copy of the License at
+           Licensed under the Apache License, Version 2.0 (the "License");
+           you may not use this file except in compliance with the License.
+           You may obtain a copy of the License at
 
-	       http://www.apache.org/licenses/LICENSE-2.0
+               http://www.apache.org/licenses/LICENSE-2.0
+        
+           Unless required by applicable law or agreed to in writing, software
+           distributed under the License is distributed on an "AS IS" BASIS,
+           WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+           See the License for the specific language governing permissions and
+           limitations under the License.
 
-	CASES and BEE SECURE are registered trademarks of SMILE GIE, all rights reserved
-	********************************************************************************/
+        CASES and BEE SECURE are registered trademarks of SMILE GIE, all rights reserved
+        ********************************************************************************/
 	require_once "../include/conf.php";
 	require_once "../translations/translations.php";
 	require_once "../include/functions.php";
--- translations/de.php
+++ translations/de.php
@@ -1,6 +1,6 @@
 <?php
 	/*******************************************************************************
-	Copyright 2010-2011 CASES Luxembourg (www.cases.lu)
+	Copyright 2011 CASES Luxembourg (www.cases.lu)
 	Copyright 2011 SMILE GIE
 
 	   Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,6 +8,12 @@
 	   You may obtain a copy of the License at
 
 	       http://www.apache.org/licenses/LICENSE-2.0
+	
+	   Unless required by applicable law or agreed to in writing, software
+	   distributed under the License is distributed on an "AS IS" BASIS,
+	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	   See the License for the specific language governing permissions and
+	   limitations under the License.
 
 	CASES and BEE SECURE are registered trademarks of SMILE GIE, all rights reserved
 	********************************************************************************/
