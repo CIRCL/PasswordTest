3d8ed7221cf0caddc51f9261b3eb0e019cf740b8
Manuel Silvoso <msilvoso@gmail.com>
2020-10-19 15:32:14 +0200

    2017 update

--- include/conf.php
+++ include/conf.php
@@ -18,27 +18,36 @@
         CASES and BEE SECURE are registered trademarks of SMILE GIE, all rights reserved
         ********************************************************************************/	
 
+	/* ----------------------- CONFIGURATION ----------------------- */
+
 	// Estimation des puissances en nombre de combinaisons par seconde testées (en FLOPS)
 	//$puissance_standard = 1000000; // estimation
-	$puissance_standard = 12400000000; // estimation (Source BOINC -> Manu)
+	//$puissance_standard = 12400000000; // estimation (Source BOINC -> Manu) 2009
+	//$puissance_standard = 16080499055; // estimation (Source BOINC -> Manu) 2013
+	//$puissance_standard = 146209685715; // estimation (Source BOINC -> Manu) 2016
+	$puissance_standard = 19593980442; // estimation (Source BOINCSTATS -> Manu credit/200 pour avoir les GFlops) novembre 2017
 
 
 	$nombre_machines_dans_botnet = 2500; // estimation
-	$puissance_distribuee = $puissance_standard*$nombre_machines_dans_botnet;
+	$puissance_distribuee = $puissance_standard * $nombre_machines_dans_botnet;
 
 
 	// http://www.top500.org/list/2007/11/100
 	//$puissance_top500_number_one = "280600000000000"; // Puissance en juin 2006
 	//$puissance_top500_number_one = "478200000000000"; // Puissance en novembre 2007
-	$puissance_top500_number_one = "1750000000000000"; // Puissance en novembre 2009
+	//$puissance_top500_number_one = "1750000000000000"; // Puissance en novembre 2009
+	//$puissance_top500_number_one = "34000000000000000"; // Puissance en septembre 2013
+	$puissance_top500_number_one = "93014600000000000"; // Puissance en juin 2016 - novembre 2017
 
 
 	// http://www.top500.org/lists/2007/11/performance_development
 	//$puissance_totalecomputing= 293000000000000; // Puissance en 2002
 	//$puissance_totalecomputing = "2790050000000000"; // Puissance en juin 2006
 	//$puissance_totalecomputing = "6965082000000000"; // Puissance en novembre 2007
-	$puissance_totalecomputing = "27600000000000000"; // Puissance en novembre 2009
-
+	//$puissance_totalecomputing = "27600000000000000"; // Puissance en novembre 2009
+	//$puissance_totalecomputing = "223000000000000000"; // Puissance en septembre 2013
+	//$puissance_totalecomputing = "593400000000000000"; // Puissance en juin 2016
+	$puissance_totalecomputing = "845121000000000000"; // Puissance en novembre 2017
 
 	// on estime qu'en moyenne on trouve un mot de passe après avoir fait la moitié des tentatives de bruteforce
 	$facteur_chance = 0.5;
